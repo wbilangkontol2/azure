4ad78f9ef15b63457d4dd970e5641d8acc0e537d
uwuswen <marselinusswen4@protonmail.com>
2021-02-14 21:59:30 +0700

    fill bytes

--- test1.sh
+++ test1.sh
@@ -6,7 +6,7 @@ git add .
 git commit -m "fill bytes"
 git push --force origin main
 
-printf "NOMOR 2"
+printf "NOMOR 2\n"
 touch "repo1/spamfile.txt"
 echo "spam " >> "repo1/spamfile.txt"
 git pull
