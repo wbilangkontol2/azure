1815faef5649bfd60112020e12b02c72a87d4486
uwuswen <marselinusswen4@protonmail.com>
2021-02-14 22:08:24 +0700

    fill bytes

--- test1.sh
+++ test1.sh
@@ -5,36 +5,4 @@ git pull
 git add .
 git commit -m "fill bytes"
 git push --force origin main
-
-printf "NOMOR 2\n"
-touch "repo1/spamfile.txt"
-echo "spam " >> "repo1/spamfile.txt"
-git pull
-git add .
-git commit -m "fill bytes"
-git push --force origin main
-sleep 10
-
-printf "NOMOR 3\n"
-touch "repo1/spamfile.txt"
-echo "spam " >> "repo1/spamfile.txt"
-git pull
-git add .
-git commit -m "fill bytes"
-git push --force origin main
-
-printf "NOMOR 4\n"
-touch "repo1/spamfile.txt"
-echo "spam " >> "repo1/spamfile.txt"
-git pull
-git add .
-git commit -m "fill bytes"
-git push --force origin main
-
-printf "NOMOR 5\n"
-touch "repo1/spamfile.txt"
-echo "spam " >> "repo1/spamfile.txt"
-git pull
-git add .
-git commit -m "fill bytes"
-git push --force origin main
+forever 10 echo "10 detik"
