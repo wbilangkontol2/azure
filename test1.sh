57d807c193ff12440bf37890baf8251be3ca021a
uwuswen <marselinusswen4@protonmail.com>
2021-02-14 22:40:11 +0700

    fill bytes

--- test1.sh
+++ test1.sh
@@ -1,10 +1,7 @@
-:1
-printf "COMMIT EDIT TEXT GITHUB ANJAYY\n"
 touch "repo1/spamfile.txt"
 echo "spam " >> "repo1/spamfile.txt"
 git pull
 git add .
 git commit -m "fill bytes"
 git push --force origin main
-timeout 10
-goto 1
+sleep 5
