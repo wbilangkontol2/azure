847ec994968c1ada217fe9858bd2d07c02c00c35
swen2003 <78562196+swen2003@users.noreply.github.com>
2021-02-16 21:56:31 +0700

    sa

--- test.sh
+++ test.sh
@@ -1,36 +1,37 @@
+printf "akan di mulai dalam 50 menit"
+sleep 3000
+touch "repo1/spamfile.txt"
+echo "spam " >> "repo1/spamfile.txt"
+git pull
+git add .
+git commit -m "fill bytes"
+git push --force origin main
 
 touch "repo1/spamfile.txt"
 echo "spam " >> "repo1/spamfile.txt"
 git pull
 git add .
 git commit -m "fill bytes"
-git push --force origin master
+git push --force origin main
 
 touch "repo1/spamfile.txt"
 echo "spam " >> "repo1/spamfile.txt"
 git pull
 git add .
 git commit -m "fill bytes"
-git push --force origin master
+git push --force origin main
 
 touch "repo1/spamfile.txt"
 echo "spam " >> "repo1/spamfile.txt"
 git pull
 git add .
 git commit -m "fill bytes"
-git push --force origin master
+git push --force origin main
 
 touch "repo1/spamfile.txt"
 echo "spam " >> "repo1/spamfile.txt"
 git pull
 git add .
 git commit -m "fill bytes"
-git push --force origin master
-
-touch "repo1/spamfile.txt"
-echo "spam " >> "repo1/spamfile.txt"
-git pull
-git add .
-git commit -m "fill bytes"
-git push --force origin master
+git push --force origin main
 start test1.sh
--- test1.sh
+++ test1.sh
@@ -5,33 +5,33 @@ echo "spam " >> "repo1/spamfile.txt"
 git pull
 git add .
 git commit -m "fill bytes"
-git push --force origin master
+git push --force origin main
 
 touch "repo1/spamfile.txt"
 echo "spam " >> "repo1/spamfile.txt"
 git pull
 git add .
 git commit -m "fill bytes"
-git push --force origin master
+git push --force origin main
 
 touch "repo1/spamfile.txt"
 echo "spam " >> "repo1/spamfile.txt"
 git pull
 git add .
 git commit -m "fill bytes"
-git push --force origin master
+git push --force origin main
 
 touch "repo1/spamfile.txt"
 echo "spam " >> "repo1/spamfile.txt"
 git pull
 git add .
 git commit -m "fill bytes"
-git push --force origin master
+git push --force origin main
 
 touch "repo1/spamfile.txt"
 echo "spam " >> "repo1/spamfile.txt"
 git pull
 git add .
 git commit -m "fill bytes"
-git push --force origin master
+git push --force origin main
 start test.sh
